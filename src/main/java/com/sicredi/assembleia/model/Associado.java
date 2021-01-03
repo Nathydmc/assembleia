7e74ea488bf8b812df87781bbc7050efc2e3b3f0
Nathália Crestani <nathaliadmc@hotmail.com>
2021-01-03 16:26:16 -0300

    Adicionados construtores ao Associado

--- src/main/java/com/sicredi/assembleia/model/Associado.java
+++ src/main/java/com/sicredi/assembleia/model/Associado.java
@@ -5,12 +5,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Associado {
 
 	@Id
